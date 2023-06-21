cf29b42476a665730cf7e1c8dcde9f6704aedee5
isacolakoglu <85408010+isacolakoglu@users.noreply.github.com>
2023-06-21 12:04:39 +0300

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CesiumDirective } from './directives/cesium.directive';
-import { CesiumComponent } from './cesium/cesium.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CesiumDirective,
-    CesiumComponent
   ],
   imports: [
     BrowserModule,
